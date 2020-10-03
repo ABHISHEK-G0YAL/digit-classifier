7b57529d4cc183aff09f6b243878b87f978a1690
Abhishek Goyal <abhishek252167@gmail.com>
2020-10-04 04:32:25 +0530

    moved text data to seprate file

---
+++ skip-gram_toy-model/toy_data.py
@@ -0,0 +1,123 @@
+texts = [
+    '''The future king is the prince
+    Daughter is the princess
+    Son is the prince
+    Only a man can be a king
+    Only a woman can be a queen
+    The princess will be a queen
+    Queen and king rule the realm
+    The prince is a strong man
+    The princess is a beautiful woman
+    The royal family is the king and queen and their children
+    Prince is only a boy now
+    A boy will be a man''',
+
+    '''Tokyo likes funny stories
+    She likes to hear funny stories
+    She likes to tell funny stories
+    She told her mom a funny story
+    When she finished she waited for her mom to laugh
+    Mom why are not you laughing
+    That was a funny story Tokyo said
+    Oh I am sorry her mom said
+    Sometimes you think something is funny but someone else thinks it is not funny
+    So Tokyos mom did not laugh at Tokyos story
+    Tokyo told the same story to her younger sister
+    Her younger sister laughed at the story''',
+
+    '''First I wake up
+    Then I get dressed
+    I walk to school
+    I do not ride a bike
+    I do not ride the bus
+    I like to go to school
+    It rains
+    I do not like rain
+    I eat lunch
+    I eat a sandwich and an apple
+    I play outside
+    I like to play
+    I read a book
+    I like to read books
+    I walk home
+    I do not like walking home
+    My mother cooks soup for dinner
+    The soup is hot
+    Then I go to bed
+    I do not like to go to bed''',
+
+    '''Jack was hungry
+    He walked to the kitchen
+    He got out some eggs
+    He took out some oil
+    He placed a skillet on the stove
+    Next he turned on the heat
+    He poured the oil into the skillet
+    He cracked the eggs into a bowl
+    He stirred the eggs
+    Then he poured them into the hot skillet
+    He waited while the eggs cooked
+    They cooked for two minutes
+    He heard them cooking
+    They popped in the oil
+    Next Jack put the eggs on a plate
+    He placed the plate on the dining room table
+    Jack loved looking at his eggs
+    They looked pretty on the white plate
+    He sat down in the large wooden chair
+    He thought about the day ahead
+    He ate the eggs with a spoon
+    They were good
+    He washed the plate with dishwashing soap
+    Then he washed the pan
+    He got a sponge damp
+    Finally he wiped down the table
+    Next Jack watched TV''',
+
+    '''Maria was learning to add numbers
+    She liked to add numbers
+    It was easy to add numbers
+    She could add one and one
+    She knew that one and one are two
+    She knew that two and two are four
+    She knew that three and three are six
+    But that was it
+    She did not know what four and four are
+    She asked her mom
+    Her mom told her that four and four are eight
+    Oh now I know Maria said
+    I am four years old now
+    In four more years I will be eight
+    Maria was a fast learner
+    She was not a slow learner''',
+
+    '''Elliot a brilliant but highly unstable young cybersecurity engineer and vigilante hacker becomes a key figure in a complex game of global dominance when he and his shadowy allies try to take down the corrupt corporation he works for
+    Elliot is a brilliant introverted young programmer who works as a cybersecurity engineer by day and vigilante hacker by night
+    He also happens to be suffering from a strange condition similar to schizophrenia which he futilely tries to keep under control by regularly taking both legal and illegal drugs and visiting his therapist
+    When a strange feisty young woman named Darlene and a secretive middle-aged man calling himself MrRobot who claims to be the mysterious leader of an underground hacking group known as F-Society offer Elliot a chance to take his vigilantism to the next level and help them take down E-Corp the corrupt multi-national financial company that Elliot works for and likes to call Evil Corp Elliot finds himself at the crossroads
+    MrRobot who has personal reasons for wanting to take down E-Corp also reveals that he already has one ally an even more mysterious secretive and highly dangerous shadowy hacking group known only as Dark Army
+    Meanwhile Elliot 's childhood and only friend Angela who blames E-Corp for the death of their parents tries to take down E-Corp legally by joining their ranks and trying to dig up evidence of their corruption from the inside
+    A wild card in this scheme becomes Tyrell Wellick an unhinged psychopathic E-Corp yuppie originally from Scandinavia who has a very unusual relationship with his dominant and ambitious wife Joanna
+    After many twists and turns MrRobot 's plan is finally put in motion with catastrophic unintended results
+    But that 's just the end of the beginning of the real story
+    MrRobot follows Elliot a young programmer who works as a cybersecurity engineer by day and as a vigilante hacker by night
+    Elliot finds himself at a crossroads when the mysterious leader of an underground hacker group recruits him to destroy the firm he is paid to protect
+    Compelled by his personal beliefs Elliot struggles to resist the chance to take down the multinational CEOs he believes are running and ruining the world
+    Eventually he realizes that a global conspiracy does exist but not the one he expected
+    Young antisocial computer programmer Elliot works as a cybersecurity engineer during the day but at night he is a vigilante hacker
+    He is recruited by the mysterious leader of an underground group of hackers to join their organization
+    Elliot 's task is to Help bring down corporate America including the company he is paid to protect which presents him with a moral dilemma
+    Although he works for a corporation his personal beliefs make it hard to resist the urge to take down the heads of multinational companies that he believes are running and ruining the world
+    In the year 2015 an incredibly unstable cybersecurity engineer named Elliot is recruited by a shady group of hackers to bring down the most powerful corporation in the history of civilization meanwhile a dark project whose purpose could bring about profound implications for society itself nears its completion
+    Elliot 's internal struggles surrounding his upbringing and personal life intertwine with his present external challenges as part of fsociety
+    Being very introverted Elliot fails to express emotions and determine for himself what is real and what is not a question that is also left somewhat ambiguous to the viewer
+    In particular Elliot struggles to remember important facts about his close relatives
+    Elliot is ultimately revealed to be partly responsible for the death of neighbour and close friend Shayla though accidental
+    His search for other chemicals to mix with his morphine addiction led Shayla to become reliant on a criminal psychopath
+    Elliot manages to help imprison the criminal before being forced to help free him on the promise that Shayla would be left unharmed a promise that was not kept
+    Meanwhile in Evil Corp Tyrell Wellick is desperate to be promoted to CTO
+    The determined young employee also appears to have an unusual relationship with his similarly ambitious wife with his pending promotion their only real common purpose
+    Failing to get the promotion out of desperation Tyrell kills the new CTO 's wife showing increasingly psychopathic tendencies
+    Eventually with the hard work of fsociety and with help from other hackers in China the large scale hack on Evil Corp is carried out successfully and all debt data held by the giant corporation is lost
+    it 's hard to know exactly to what extent Elliot is himself wholly fsociety but the group certainly appears to be ultimately based mainly around Elliot and his sister Darlene'''
+]
